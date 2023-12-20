da4ee0ac4c32164b634fde56d0a29d0ec7ac94c4
elyse502 <elyseniyibizi502@gmail.com>
2023-12-20 07:01:47 -0800

    Initial commit

--- person.py
+++ person.py
@@ -1,14 +1,13 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 
 class Person:
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-	def __str__(self):
-		return f"I'm {self.name}, and I'm {self.age} years old."
-	
-	def __repr__(self):
-		return f"{type(self).__name__}(name='{self.name}', age={self.age})"
-		
+    def __str__(self):
+        return f"I'm {self.name}, and I'm {self.age} years old."
+
+    def __repr__(self):
+        return f"{type(self).__name__}(name='{self.name}', age={self.age})"
