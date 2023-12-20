3572b6c5abbb96a56f739bfc5067d2f3073df501
elyse502 <elyseniyibizi502@gmail.com>
2023-12-20 06:01:29 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+
+from person import Person
+
+
+jane = Person("Jane Doe", 25)
+print(f"{jane!s}")
+print(f"{jane!r}")
---
+++ test.py
@@ -0,0 +1,15 @@
+class Robot:
+    def __init__(self, name=None):
+        self.name = name
+
+    def say_hi(self):
+        if self.name:
+            print("Hi, I am {}".format(self.name))
+        else:
+            print("Hi, I am a robot without a name")
+
+
+x = Robot()
+x.say_hi()
+y = Robot("Marvin")
+y.say_hi()
